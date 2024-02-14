a28567730703e5930ae292c3ab964e8af97ca11e
Antony Henrique Bresolin <40371352+AntonyBresolin@users.noreply.github.com>
2024-02-14 18:27:48 -0300

    #c1 - Estrutura inicial do projeto
    Estrutura inicial do projeto.
    Pastas e framework base.
    
    frontend:
    - React jsx e tailwind.
    
    backend:
    - Spring boot - JAVA
    - Mysql

---
+++ backend/backend-catalog/src/main/java/com/antonybresolin/backendcatalog/BackendCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.antonybresolin.backendcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCatalogApplication.class, args);
+	}
+
+}
---
+++ backend/backend-catalog/src/test/java/com/antonybresolin/backendcatalog/BackendCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonybresolin.backendcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
